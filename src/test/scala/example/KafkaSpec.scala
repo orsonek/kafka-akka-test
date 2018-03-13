f8b85f608e8f699a5ab0bd32d712fd7a73b5b652
cane <cane@iqtech.pl>
2018-03-13 19:25:21 +0100

    Sending and reading to/from internal Kafka. Test has strict amount of time set to complete (20s).

--- src/test/scala/example/KafkaSpec.scala
+++ src/test/scala/example/KafkaSpec.scala
@@ -4,13 +4,21 @@ import java.io.File
 import java.nio.file.Files
 import java.util.Properties
 
-import kafka.server.KafkaConfig
-import kafka.server.KafkaServerStartable
-
+import akka.actor.ActorSystem
+import akka.kafka.scaladsl.{Consumer, Producer}
+import akka.kafka.{ConsumerSettings, ProducerSettings, Subscriptions}
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Sink, Source}
+import kafka.server.{KafkaConfig, KafkaServerStartable}
 import org.apache.curator.framework.{CuratorFramework, CuratorFrameworkFactory}
-import org.apache.curator.test.TestingServer
 import org.apache.curator.retry.ExponentialBackoffRetry
+import org.apache.curator.test.TestingServer
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.serialization.{ByteArrayDeserializer, ByteArraySerializer, StringDeserializer, StringSerializer}
 import org.scalatest._
+
+import scala.concurrent.Future
 
 class KafkaSpec extends FlatSpec with Matchers with BeforeAndAfterAll {
     val ZOOKEEPER_HOST = "localhost"
@@ -18,11 +26,17 @@ class KafkaSpec extends FlatSpec with Matchers with BeforeAndAfterAll {
     val KAFKA_BROKER_HOST = "localhost"
     val KAFKA_BROKER_PORT = 19091
 
-    implicit var logDir: File = _
+
+    implicit var system: ActorSystem = _
+    implicit var materializer: ActorMaterializer = _
+
+    implicit var kafkaLogDir: File = _
 
     implicit var zooKeeper: TestingServer = _
     implicit var kafka: KafkaServerStartable = _
     implicit var curator: CuratorFramework = _
+
+    import scala.concurrent.ExecutionContext.Implicits.global
 
     // ---------------------------------------------------------------------------------------------------------------------
     // LIFECYCLE   LIFECYCLE   LIFECYCLE   LIFECYCLE   LIFECYCLE   LIFECYCLE   LIFECYCLE   LIFECYCLE   LIFECYCLE   LIFECYCLE
@@ -32,9 +46,14 @@ class KafkaSpec extends FlatSpec with Matchers with BeforeAndAfterAll {
       * Test lifecycle - before
       */
     override def beforeAll( ): Unit = {
+
+        // start actor system
+        system = ActorSystem( "akka-zoo-kafka" )
+        materializer = ActorMaterializer()( system )
+
         // log directory removed after exit
-        logDir = Files.createTempDirectory( "kafka" ).toFile
-        logDir.deleteOnExit()
+        kafkaLogDir = Files.createTempDirectory( "kafka-tmp-dev-logs" ).toFile
+        kafkaLogDir.deleteOnExit()
 
         // zoo..
         zooKeeper = new TestingServer( ZOOKEEPER_PORT )
@@ -46,11 +65,11 @@ class KafkaSpec extends FlatSpec with Matchers with BeforeAndAfterAll {
         kafkaProperties.setProperty( "broker.id", "1" )
         kafkaProperties.setProperty( "host.name", s"$KAFKA_BROKER_HOST" )
         kafkaProperties.setProperty( "port", Integer.toString( KAFKA_BROKER_PORT ) )
-        kafkaProperties.setProperty( "log.dir", logDir.getAbsolutePath )
+        kafkaProperties.setProperty( "log.dir", kafkaLogDir.getAbsolutePath )
         kafkaProperties.setProperty( "log.flush.interval.messages", String.valueOf( 1 ) )
         kafkaProperties.setProperty( "delete.topic.enable", String.valueOf( true ) )
         kafkaProperties.setProperty( "offsets.topic.replication.factor", String.valueOf( 1 ) )
-        kafkaProperties.setProperty( "auto.create.topics.enable", String.valueOf( false ) )
+        kafkaProperties.setProperty( "auto.create.topics.enable", String.valueOf( true ) )
 
         kafka = new KafkaServerStartable( new KafkaConfig( kafkaProperties ) )
         kafka.startup()
@@ -59,7 +78,7 @@ class KafkaSpec extends FlatSpec with Matchers with BeforeAndAfterAll {
         curator = CuratorFrameworkFactory.builder()
             .retryPolicy( new ExponentialBackoffRetry( 1000, 3 ) )
             .namespace( "tests" )
-            .connectString( s"127.0.0.1:$ZOOKEEPER_PORT" ).build()
+            .connectString( s"$ZOOKEEPER_HOST:$ZOOKEEPER_PORT" ).build()
         curator.start()
     }
 
@@ -67,10 +86,13 @@ class KafkaSpec extends FlatSpec with Matchers with BeforeAndAfterAll {
       * Test lifecycle - after
       */
     override def afterAll( ): Unit = {
+        Thread.sleep( 20000 )
+        //system
         kafka.shutdown()
         curator.close()
         zooKeeper.stop()
     }
+
 
 
     // ---------------------------------------------------------------------------------------------------------------------
@@ -79,15 +101,45 @@ class KafkaSpec extends FlatSpec with Matchers with BeforeAndAfterAll {
 
     // ZOO Keeper test
 
-    it must "create new node" in {
-        curator.create().orSetData().forPath( "/iqtech", "THIS IS SPARTA".getBytes() )
+    it must "create zk node" in {
+        curator.create().orSetData().forPath( "/sparta", "THIS IS SPARTA".getBytes() )
     }
 
-    it must "read created node" in {
-        val d: Array[ Byte ] = curator.getData.forPath( "/iqtech" )
+    it must "read created zk node" in {
+        val d: Array[ Byte ] = curator.getData.forPath( "/sparta" )
         val v = new String( d )
-        println( v )
         assert( v.equals( "THIS IS SPARTA" ) )
+    }
+
+    it must "produce some messages to topic" in {
+        val producerSettings = ProducerSettings( system, new ByteArraySerializer, new StringSerializer )
+            .withBootstrapServers( s"$KAFKA_BROKER_HOST:$KAFKA_BROKER_PORT" )
+
+        Source( 1 to 100 )
+            .map( i => s"King's Announcement #$i" )
+            .map { elem =>
+                new ProducerRecord[ Array[ Byte ], String ]( "sparta", elem )
+            }
+            .runWith( Producer.plainSink( producerSettings ) )
+    }
+
+    it must "read some messages from topic (commitable in Kafka)" in {
+
+        val consumerSettings = ConsumerSettings( system, new ByteArrayDeserializer, new StringDeserializer )
+            .withBootstrapServers( s"$KAFKA_BROKER_HOST:$KAFKA_BROKER_PORT" )
+            .withGroupId( "people-of-sparta" )
+            .withProperty( ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest" )
+
+        // source may be committableSource, plainSource or atMostOnceSource
+        Consumer.committableSource( consumerSettings, Subscriptions.topics( "sparta" ) )
+            .mapAsync( 1 ) { msg =>
+                println( msg.record.value )
+                Future( msg )
+            }
+            .mapAsync( 1 ) { msg =>
+                msg.committableOffset.commitScaladsl()
+            }
+            .runWith( Sink.ignore )
     }
 
     // TODO Kafka test
